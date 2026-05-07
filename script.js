c5f37dcc56b0d29b125b05c4957fa350e66d82f7
AshokVangapandu <ashokvangapandu45@gmail.com>
2026-05-07 12:56:51 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Portfolio Loaded");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("mousemove", (event) => {
+    const rect = button.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    button.style.setProperty("--x", `${x}px`);
+    button.style.setProperty("--y", `${y}px`);
+  });
+});
+
+console.log("Luxury portfolio loaded");
