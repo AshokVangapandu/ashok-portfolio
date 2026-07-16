471cd38ac73edc94819a0f9c53e5bee5521d9786
AshokVangapandu <ashokvangapandu45@gmail.com>
2026-07-16 16:44:07 +0000

    Deploying to gh-pages from @ AshokVangapandu/ashok-portfolio@2c6eeb5735dd9b49d54f31864d3af15a40c6fc26 🚀

---
+++ assets/Mendix-Brandmark-25F8q1hN.js
@@ -0,0 +1,1 @@
+const s="/ashok-portfolio/assets/SCSS-CikPBs4p.png",o="/ashok-portfolio/assets/Mendix-Brandmark-B0FigQz2.webp";export{o as m,s};
--- assets/Mendix-Brandmark-DT8dFSh_.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("SCSS-CikPBs4p.png",import.meta.url).href,o=""+new URL("Mendix-Brandmark-B0FigQz2.webp",import.meta.url).href;export{o as m,e as s};
