113e0b89ba00c3c40139222137508f32f15de291
Ashok Vangapandu <ashokvangapandu45@gmail.com>
2026-07-22 23:07:38 +0530

    Chnages on Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/ashok-portfolio/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
